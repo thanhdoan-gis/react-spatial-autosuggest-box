a30d6fd5deeb8e035dc97b7b943da15b0a723d8f
thanhdoan-gis <thanhdoan.gis@gmail.com>
2026-02-04 21:47:31 +0700

    Init project with Vite React TS TailWindCSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        Hello What3Words!
+      </div>
+    </>
+  )
+}
+
+export default App
